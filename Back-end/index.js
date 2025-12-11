35a10e8a013d4653feba6d0e459624599c3e5b46
Anshuman Tyagi <161432160+Anshumantyagi2004@users.noreply.github.com>
2025-12-11 10:57:55 +0530

    Update index.js

--- Back-end/index.js
+++ Back-end/index.js
@@ -3,29 +3,26 @@ import dotenv from 'dotenv';
 import connectDB from "./src/config/db.js";
 import cors from 'cors';
 import studentRoutes from "./src/routes/studentRoute.js";
-import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "https://student-dashboard-wq1c.vercel.app"
-  ],
-  methods: ["GET", "POST"]
+    origin: [
+        "https://student-dashboard-wq1c.vercel.app"
+    ],
+    methods: ["GET", "POST"]
 }));
 app.get("/", (req, res) => {
-  res.json({
-    msg: "Backend is running",
-  
-  });
+    res.json({
+        msg: "Backend is running",
+
+    });
 });
 app.use(express.json());
-connectDB();   
+connectDB();
 
 
 app.use("/api", studentRoutes);
-  app.listen(5000, () => {
+app.listen(5000, () => {
     console.log("Server running on port 5000");
-  });
-
-
+});
