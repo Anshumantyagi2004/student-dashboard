429458f98a16ff3ae86f5ff8928bd97aecb95b48
Anshuman Tyagi <161432160+Anshumantyagi2004@users.noreply.github.com>
2025-12-11 09:22:15 +0530

    Update db.js

--- Back-end/src/config/db.js
+++ Back-end/src/config/db.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
-import Student from"../models/student.js";
+import Student from "../models/student.js";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.URI);
+
     console.log("MongoDB Connected!");
 
     await Student.createCollection();
-    console.log("Done");
-    return "OK"
 
+    return "Connected";   
   } catch (err) {
     console.error("MongoDB Error:", err.message);
-    process.exit(1);
+    return "Not Connected"; 
   }
 };
 
